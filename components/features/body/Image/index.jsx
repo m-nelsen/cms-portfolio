adecba08b238faf679334aeb34822a1ecb7293fa
Matt Nelsen <mattnelsen95@gmail.com>
2022-03-30 21:35:33 -0700

    Renaming Image feature to ImageTag

--- components/features/body/Image/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Image = (props) => {
-  return <>Image feature placeholder</>;
-};
-
-export default Image;
---
+++ components/features/body/ImageTag/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ImageTag = (props) => {
+  return <>ImageTag feature placeholder</>;
+};
+
+export default ImageTag;
