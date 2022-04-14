097c789e7dee680fd8b2120aad9aa05e3c3970c6
Matt Nelsen <mattnelsen95@gmail.com>
2022-04-13 21:37:42 -0700

    Adding update route for template api

--- pages/api/templateById/[id].js
+++ pages/api/templateById/[id].js
@@ -16,6 +16,23 @@ const templateFindById = async (req, res) => {
         res.status(400).json({ success: false });
       }
       break;
+
+    case "PUT":
+      try {
+        const data = await Template.findOneAndUpdate(
+          { _id: id },
+          JSON.parse(req.body),
+          {
+            upsert: true,
+          }
+        );
+        res.status(200).json({ sucess: true, data });
+      } catch (error) {
+        console.error(error);
+        res.status(400).json({ success: false });
+      }
+      break;
+
     default:
       res.status(400).json({ success: false });
       break;
