c44fde3cdb5e212f5db65f86512634884bd4b8b2
Matt Nelsen <mattnelsen95@gmail.com>
2022-04-17 23:17:49 -0700

    Including font awesome globally

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import "../styles/globals.css";
 import "bulma/css/bulma.css";
+import "../public/fontawesome/css/all.css";
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
